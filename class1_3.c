ada6d61018393f78de6cf8d48f44b0d8296b3dc2
Priyanshu Mondal <131907694+PriyanshuMondal@users.noreply.github.com>
2024-02-09 01:13:14 +0530

    class1_3_2.c

--- class1_3.c
+++ class1_3.c
@@ -4,21 +4,28 @@
 
 int main()
 {
-    char str1[Max], str2[Max], c;
+    char str1[Max], str2[Max], str3[Max], c;
     printf("Enter the first string :");
     gets(str1);
     printf("Enter the character :");
     scanf("%c", &c);
     printf("Enter the string to be replaced with :");
     scanf("%s", &str2);
+    int k = 0;
     for (int i = 0; i < strlen(str1); i++)
     {
         if (str1[i] == c)
         {
-            printf("%s", str2);
+            for (int i = 0; i < strlen(str2); i++)
+            {
+                str3[k] = str2[i];
+                k++;
+            }
             continue;
         }
-        printf("%c", str1[i]);
+        str3[k] = str1[i];
+        k++;
     }
+    puts(str3);
     return 0;
 }
