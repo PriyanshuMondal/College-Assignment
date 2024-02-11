fcdb2a5134520dfc8304466e78fa4409e70ea7d7
Priyanshu Mondal <131907694+PriyanshuMondal@users.noreply.github.com>
2024-02-11 13:13:21 +0530

    class2_1.3.c
    This is for scenario where the length of roll no is different
    which should not be in general case scenario

--- class2_1.c
+++ class2_1.c
@@ -10,7 +10,12 @@ typedef struct
 
 void compareStudents(void *rec1, void *rec2)
 {
-    int ans = strcmp((char *)rec1, (char *)rec2);
+    int ans;
+    if (strlen((char *)rec1) > strlen((char *)rec2))
+        ans = 1;
+    if (strlen((char *)rec1) < strlen((char *)rec2))
+        ans = -1;
+    ans = strcmp((char *)rec1, (char *)rec2);
     printf("%d\n", ans);
 }
 
@@ -26,7 +31,6 @@ int main()
         printf("student %d\n", i + 1);
         scanf("%s %s %d", S[i].name, S[i].roll_no, &S[i].score);
     }
-
     printf("Enter the students to compare :");
     scanf("%d %d", &s1, &s2);
     compareStudents(&S[s1 - 1].roll_no, &S[s2 - 1].roll_no);
